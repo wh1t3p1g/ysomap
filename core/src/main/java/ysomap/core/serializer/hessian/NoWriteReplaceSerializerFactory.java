dd2e58149ae69a6fe58ec8b20c76ead7f4eaae55
wh1t3p1g <wh1t3p1g@gmail.com>
2022-04-15 16:38:32 +0800

    fix bugs

--- core/src/main/java/ysomap/core/serializer/hessian/NoWriteReplaceSerializerFactory.java
+++ core/src/main/java/ysomap/core/serializer/hessian/NoWriteReplaceSerializerFactory.java
@@ -3,25 +3,10 @@ package ysomap.core.serializer.hessian;
 import com.caucho.hessian.io.*;
 
 public class NoWriteReplaceSerializerFactory extends SerializerFactory {
-    /**
-     * {@inheritDoc}
-     *
-     * @see com.caucho.hessian.io.SerializerFactory#getObjectSerializer(java.lang.Class)
-     */
-    @Override
-    public com.caucho.hessian.io.Serializer getObjectSerializer (Class<?> cl ) throws HessianProtocolException {
-        return super.getObjectSerializer(cl);
-    }
 
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see com.caucho.hessian.io.SerializerFactory#getSerializer(java.lang.Class)
-     */
     @Override
     public com.caucho.hessian.io.Serializer getSerializer (Class cl ) throws HessianProtocolException {
-        com.caucho.hessian.io.Serializer serializer = super.getSerializer(cl);
+        Serializer serializer = super.getSerializer(cl);
 
         if ( serializer instanceof WriteReplaceSerializer) {
             return UnsafeSerializer.create(cl);
