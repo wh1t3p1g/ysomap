8da6b2176e3d65faa608686a8cd7cfa77a965b90
Ar3h <Ar3h@Ar3hang1.gmail.com>
2022-11-07 01:22:58 +0800

    add payload DetectClass, add bullet DetectClassBullet

---
+++ core/src/main/java/ysomap/payloads/java/jdk/DetectClass.java
@@ -0,0 +1,33 @@
+package ysomap.payloads.java.jdk;
+
+import ysomap.bullets.Bullet;
+import ysomap.bullets.jdk.DetectClassBullet;
+import ysomap.common.annotation.Dependencies;
+import ysomap.common.annotation.Payloads;
+import ysomap.common.annotation.Require;
+import ysomap.common.annotation.Targets;
+import ysomap.payloads.AbstractPayload;
+
+/**
+ * DetectClass 通过 dnslog 探测存在哪些类
+ * 不依赖jdk版本
+ *
+ * @author Ar3h
+ * @since 2022/11/06
+ */
+@SuppressWarnings({"rawtypes", "unchecked"})
+@Payloads
+@Targets({Targets.JDK})
+@Dependencies({"*"})
+@Require(bullets = {"DetectClassBullet"}, param = false)
+public class DetectClass extends AbstractPayload<Object> {
+    @Override
+    public Bullet getDefaultBullet(Object... args) throws Exception {
+        return DetectClassBullet.newInstance(args);
+    }
+    
+    @Override
+    public Object pack(Object obj) throws Exception {
+        return obj;
+    }
+}
